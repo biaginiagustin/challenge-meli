ccaa508d6c75c5e8a8f58ac7d5715b95c0e3cf9d
Agustin Biagini <biaginiagustin@gmail.com>
2022-07-24 22:26:12 -0300

    fix

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,10 +1,11 @@
 require 'active_record'
+require 'erb'
 
 ENV['APP_ENV'] ||= 'development'
 
 def db_configuration
     db_configuration_file = File.join(File.expand_path('..', __FILE__), 'db', 'config.yml')
-    YAML.load(File.read(db_configuration_file))
+    YAML.load(ERB.new(db_configuration_file).result)
 end
 
 ActiveRecord::Base.establish_connection(db_configuration[ENV.fetch('APP_ENV')])
